02dd7d907d646b5e93efe650ad7b2cacf3ffa556
borzdmyt <borzdmyt@kkg-do.eu>
2024-09-23 08:28:36 +0200

    Initialer Commit für Repository - Start

---
+++ src/Konstruktor.java
@@ -0,0 +1,27 @@
+public class Konstruktor {
+
+    private int zahl1, zahl2, zahl3;
+
+    private void tueEtwas(){
+
+        zahl1 = 10;
+        zahl2 = 4;
+        zahl3 = 13;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
